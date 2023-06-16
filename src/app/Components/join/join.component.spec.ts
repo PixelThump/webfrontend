59346b7725f027fa02d574767ba6db4f93c74d38
roboter5123 <pascal.roboter@hotmail.com>
2023-06-16 20:26:13 +0200

    Added a page for joining seshs

---
+++ src/app/Components/join/join.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { JoinComponent } from './join.component';
+
+describe('JoinComponent', () => {
+  let component: JoinComponent;
+  let fixture: ComponentFixture<JoinComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [JoinComponent]
+    });
+    fixture = TestBed.createComponent(JoinComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/Pages/join-page/join-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join-page',
+  templateUrl: './join-page.component.html',
+  styleUrls: ['./join-page.component.css']
+})
+export class JoinPageComponent {
+
+}
