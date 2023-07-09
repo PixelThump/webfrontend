bdbb5fd3e91343f3bcfa8a96e95e35b4f26d9f2e
roboter5123 <pascal.roboter@hotmail.com>
2023-07-09 18:49:25 +0200

    Host now shows the player that is buzzered in

--- src/app/components/sesh/quizxel/host/quizxel-host-main/quizxel-host-main.component.ts
+++ src/app/components/sesh/quizxel/host/quizxel-host-main/quizxel-host-main.component.ts
@@ -1,12 +1,29 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {QuizxelMainState} from "../../model/QuizxelMainState";
+import {QuizxelPlayer} from "../../model/QuizxelPlayer";
 
 @Component({
   selector: 'app-quizxel-host-main',
   templateUrl: './quizxel-host-main.component.html',
   styleUrls: ['./quizxel-host-main.component.css']
 })
-export class QuizxelHostMainComponent {
+export class QuizxelHostMainComponent implements OnChanges{
 
   @Input() state = <QuizxelMainState>{}
+  buzzeredPlayer?: QuizxelPlayer
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+    console.log(this.state)
+    this.state.players.forEach(player => {
+      console.log(player)
+      console.log(this.state.buzzedPlayerId)
+      console.log(player.playerId === this.state.buzzedPlayerId)
+      if (player.playerId === this.state.buzzedPlayerId) {
+
+        this.buzzeredPlayer = player
+        console.log(player)
+      }
+    })
+  }
 }
