eb7173eb2d2505a887d30f579a0c2c9d3354a52d
roboter5123 <pascal.roboter@hotmail.com>
2023-07-20 18:30:18 +0200

    !!!!!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {HomePageComponent} from "./Pages/home-page/home-page.component";
-import {HostPageComponent} from "./Pages/host-page/host-page.component";
-import {JoinPageComponent} from "./Pages/join-page/join-page.component";
+import {HomePageComponent} from "./pages/home-page/home-page.component";
+import {HostPageComponent} from "./pages/host-page/host-page.component";
+import {JoinPageComponent} from "./pages/join-page/join-page.component";
 import {QuizxelPageComponent} from "./pages/quizxel-page/quizxel-page.component";
 
 const routes: Routes = [
