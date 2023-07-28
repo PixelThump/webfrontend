877d48eafb06d90e37def4e27b91242669ad4d44
roboter5123 <pascal.roboter@hotmail.com>
2023-07-28 16:25:22 +0200

    Corrected env value

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import {IEnvironment} from "./IEnvironment";
 
 export const environment: IEnvironment = {
   production: true,
-  apiUrl: "pixelvibe.roboter5123.com/api",
+  apiUrl: "pixelthump.win/api",
   apiProtocol: "https://",
   socketProtocol: "wss:",
   supportedSeshTypes: ["QUIZXEL"]
