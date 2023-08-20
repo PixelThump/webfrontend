e221fe4a8805d9fcbc28fd9041f76a9a632cc394
roboter5123 <pascal.roboter@hotmail.com>
2023-08-20 11:44:41 +0200

    Added a popup component

---
+++ src/app/components/quizxel/quizxel-popup/quizxel-popup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quizxel-popup',
+  templateUrl: './quizxel-popup.component.html',
+  styleUrls: ['./quizxel-popup.component.css']
+})
+export class QuizxelPopupComponent {
+
+}
